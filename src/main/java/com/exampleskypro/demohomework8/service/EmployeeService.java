943aa89344df2ae8289853dde63e76da2b7ca265
Илон <vlasovvdm@gmail.com>
2024-02-16 20:09:02 +0400

    !

--- src/main/java/com/exampleskypro/demohomework8/service/EmployeeService.java
+++ src/main/java/com/exampleskypro/demohomework8/service/EmployeeService.java
@@ -8,11 +8,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 @Service
 public class EmployeeService {
-     private final List<Employee> employees = new ArrayList<>();
-    private static final int MAX_WORKERS=100;
+    private final List<Employee> employees = new ArrayList<>();
+    private static final int MAX_WORKERS = 100;
 
     public void addWorker(String firstName, String lastName) {
         Employee worker = new Employee(firstName, lastName);
@@ -26,17 +28,31 @@ public class EmployeeService {
 
     public void deleteWorker(String firstName, String lastName) {
         Employee delWorker = new Employee(firstName, lastName);
-        employees.remove(delWorker);
+        if (employees.contains(delWorker)) {
+            employees.remove(delWorker);
+        } else {
+            throw new EmployeeNotFoundException();
+        }
+
     }
 
     public Employee findWorker(String firstName, String lastName) {
-        System.out.println(firstName+lastName);
+        System.out.println(firstName + lastName);
         for (Employee employee : employees) {
             if (employee.getFirstName().equals(firstName) && employee.getLastName().equals(lastName)) {
                 return employee;
 
             }
         }
+
         throw new EmployeeNotFoundException();
+
     }
+
+    public List<Employee> allWorkers() {
+        return employees;
+    }
+
+
+
 }
